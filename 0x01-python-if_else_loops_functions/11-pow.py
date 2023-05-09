fcdf8f269009e192168a0756569d5099ce72d498
dannycod3r <oseid7807@gmail.com>
2023-05-09 13:34:58 -0700

    Add function to compute the a to the power b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """pow - compute a to the power b, a^b
+    @a: first operand
+    @b: second operand
+    Return: a raised to the power b
+    """
+    return (a ** b)
