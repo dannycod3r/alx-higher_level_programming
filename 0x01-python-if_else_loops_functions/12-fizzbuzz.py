6f48cd68c8ed373bd34fa36f25e6b304a152f7d9
dannycod3r <oseid7807@gmail.com>
2023-05-09 14:12:08 -0700

    Feat: Good old FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     """fizzbuzz - for 1 to 100 print fizz, buzz, fizzbuzz"""
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0 :
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
             print("Fizz ", end="")
