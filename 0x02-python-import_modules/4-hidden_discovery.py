eb549c559146eae04ee72954d1e295f15f5759f0
dannycod3r <oseid7807@gmail.com>
2023-05-12 06:22:56 -0700

    Fix: Remove function

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    def discovery():
-        names = dir(hidden_4)
-        for name in names:
-            if name[:2] != "__":
-                print("{:s}".format(name))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print("{:s}".format(name))
