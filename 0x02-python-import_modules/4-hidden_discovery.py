45402afa0e53ca0c2b0c5fcf8da8cbe84923578d
dannycod3r <oseid7807@gmail.com>
2023-05-12 06:19:53 -0700

    Fix: put all under if check

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-
-def discovery():
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print("{:s}".format(name))
-
 if __name__ == "__main__":
-    discovery()
+    import hidden_4
+
+    def discovery():
+        names = dir(hidden_4)
+        for name in names:
+            if name[:2] != "__":
+                print("{:s}".format(name))
