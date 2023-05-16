c6731466ed766dda69eda2cd44f4c2c08c873359
dannycod3r <oseid7807@gmail.com>
2023-05-15 21:18:05 -0700

    Feat: Add function to replace element at idx

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    copy_list = my_list[:]
+    copy_list[idx] = element
+    return copy_list
+
+
+if __name__ == "__main__":
+    new_in_list()
