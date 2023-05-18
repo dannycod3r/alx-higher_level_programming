47d22859a2d69dffb78049a57df6a3b86c9df09f
dannycod3r <oseid7807@gmail.com>
2023-05-18 06:43:53 -0700

    Feat: Add function that adds only unique elements in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = set(my_list)
+    sum = 0
+
+    for i in new_list:
+        sum += i
+
+    return sum
+
+
+if __name__ == "__main__":
+    uniq_add(my_list)
