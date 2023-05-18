6fdd1268fdf49973aa42fd6fbcc36b83c9fe9726
dannycod3r <oseid7807@gmail.com>
2023-05-18 10:23:30 -0700

    Style: Rename variables to comform E501

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda elem: elem ** 2, row)), matrix))
+    return list(map(lambda r: list(map(lambda e: e ** 2, r)), matrix))
