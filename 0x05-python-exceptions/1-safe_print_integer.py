1e60659763703a8d9e0a0268904e0124116e5003
dannycod3r <oseid7807@gmail.com>
2023-05-29 08:40:53 -0700

    Fix: Add TypeError and ValuError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,10 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except(TypeError, ValueError):
         return False
+
+
+
+if __name__ == "__main__":
+    safe_print_integer(value)
