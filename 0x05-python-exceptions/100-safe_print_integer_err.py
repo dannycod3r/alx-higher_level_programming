6e322948021eca82d085f5fe021adcc58a4cf135
dannycod3r <oseid7807@gmail.com>
2023-05-30 04:03:14 -0700

    Feat: Add function the safely prints an interger

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as exc:
+        print("Exception:", str(exc), file=stderr)
+        return False
+
+
+if __name__ == "__main__":
+    safe_print_integer_err(value)
