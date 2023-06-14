e2e0f86298a7fe9826942e0e2f80d944c30f0697
dannycod3r <oseid7807@gmail.com>
2023-06-14 10:42:23 +0000

    Feat: Add function that return the json representation of a string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module supplies the function to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """the function returns the JSON representation of 
+    an object (string)
+    
+    Args:
+        my_obj: string"""
+    my_json = json.dumps(my_obj)
+    return my_json
