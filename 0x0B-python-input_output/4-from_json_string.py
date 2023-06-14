42e66ad655cc1e04f9cddfae2a7cc2660e0b97e2
dannycod3r <oseid7807@gmail.com>
2023-06-14 10:51:55 +0000

    Feat: Add function that converts a json serialised string to a python data structure

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module supplies the function"""
+import json
+
+def from_json_string(my_str):
+    """the function  returns an object (Python data structure) 
+    represented by a JSON string
+    
+    Args:
+        my_str: json string"""
+    my_obj = json.loads(my_str)
+    return my_obj
