6876a48c0094616bfd34bf6e63e3b2850d39e888
dannycod3r <oseid7807@gmail.com>
2023-06-14 07:25:20 -0700

    Chore, Refactor: Make files executable, use json.dumb to write to file instead file.write-> to get proper json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,14 +4,11 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """the function  writes an Object to a text file, 
+    """the function  writes an Object to a text file,
     using a JSON representation
-    
+
     Args:
         my_obj: object/python data structure
         filename: the file to be written"""
-    # convert object to json
-    my_json = json.dumps(my_obj)
-    # put to the file
     with open(filename, mode='w', encoding='utf-8') as file:
-        file.write(my_json)
+        json.dump(my_obj, file)
