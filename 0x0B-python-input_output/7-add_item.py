71a75460cc6d6d410ae1accdfd8b20c0137645d2
dannycod3r <oseid7807@gmail.com>
2023-06-14 09:39:45 -0700

    Fix: Change list += to list.extend in order fo the new elements be add to the existing list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,11 +10,12 @@ def add_item():
     save_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    args_list = []
-    args_list += argv[1:]
-
+    try:
+        args_list = load_json_file(filename)
+    except FileNotFoundError:
+        args_list = []
+    args_list.extend(argv[1:])
     save_json_file(args_list, filename)
-    load_json_file(filename)
 
 
 if __name__ == "__main__":
