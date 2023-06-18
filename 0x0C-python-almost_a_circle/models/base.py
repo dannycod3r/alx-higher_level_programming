01b3aa07340b2e3a9a92fa02acae4b70dc99ab07
dannycod3r <oseid7807@gmail.com>
2023-06-18 23:16:03 +0000

    Chore: Add empty class Base, comform to tdd, tests before implementation

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module contains the class Base"""
+
+
+class Base:
+    """Base class for all shapes
+
+    Attributes:
+        __nb_object: number of objects"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        pass
