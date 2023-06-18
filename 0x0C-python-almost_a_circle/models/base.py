3430fa6fa6fcc9d0136b2ca5a94a0df88d1e9053
dannycod3r <oseid7807@gmail.com>
2023-06-18 23:45:44 +0000

    Feat: Implemet the ability for object to track id

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,4 +10,8 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        pass
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
