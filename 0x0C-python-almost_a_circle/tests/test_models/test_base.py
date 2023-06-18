e1b9504f0f3d0224198f8c14edd32a55d4ba362f
dannycod3r <oseid7807@gmail.com>
2023-06-18 23:44:21 +0000

    Style, Docs: Add newline at end of file, add extra newline before class def, break doc string to two lines and rename test methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,20 +3,23 @@
 import unittest
 from models.base import Base
 
+
 class TestBaseClass(unittest.TestCase):
     """Test cases for the class Base
 
     """
     # init test without id
-    def test_initialization_without_an_id_increament_nb_objects(self):
-        """Initialization without id must increase the nb_object private attribue"""
+    def test_initialization_without_id_increament_nb_objects(self):
+        """Initialization without id must \
+            increase the nb_object private attribue"""
         self.assertEqual(Base().id, 1)
         self.assertEqual(Base().id, 2)
         self.assertEqual(Base().id, 3)
 
         # init test with id
-    def test_initialization_with_an_id_set_id_to_provided(self):
-        """Set the id of object to the provided one if set during initialisation"""
+    def test_initialization_with_id_set_id_to_provided(self):
+        """Set the id of object to the provided\
+            one if set during initialisation"""
         self.assertEqual(Base(2).id, 2)
         self.assertEqual(Base(10).id, 10)
         self.assertEqual(Base(100).id, 100)
