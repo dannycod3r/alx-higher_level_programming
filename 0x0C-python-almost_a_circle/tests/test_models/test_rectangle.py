4e96d8adfe25b0b8ed713e291e6e2bd16987b903
dannycod3r <oseid7807@gmail.com>
2023-06-19 00:22:22 +0000

    Test: Add test for Rectangle initialisation, should be able to set id per the base class and assign respective values in constructor

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module test for class Rectangle"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangleClass(unittest.TestCase):
+    """Test class for class Rectangle"""
+    def test_initialization_rectangle_auto_set_id(self):
+        self.assertEqual(Rectangle().id, 1)
+        self.assertEqual(Rectangle(2, 10).id, 2)
+        self.assertEqual(Rectangle(10, 2).id, 3)
+        self.assertEqual(Rectangle(10, 2, 0, 0, 12).id, 12)
