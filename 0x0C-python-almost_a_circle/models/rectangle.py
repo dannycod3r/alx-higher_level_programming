f366fb9d895c2f7ca58f8818aa97c4bc0598f86d
dannycod3r <oseid7807@gmail.com>
2023-06-19 02:30:26 +0000

    Feat: Add attribute validation for x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,56 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
+    # Getters and setter for x attribute
+    ####################################
+    @property
+    def x(self):
+        """Return the x coord of the rectangle
+
+        Args:
+            value: x coord of rectangle
+
+        Raises:
+            TypeError: if not integer
+            ValueError: if < 0"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coord of the rectangle
+
+        Args:
+            value: x coord of rectangle
+
+        Raises:
+            TypeError: if not integer
+            ValueError: if < 0"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+     # Getters and setter for y attribute
+    ####################################
+    @property
+    def y(self):
+        """Return the y coord of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coord of the rectangle
+
+        Args:
+            value: y coord of rectangle
+
+        Raises:
+            TypeError: if not integer
+            ValueError: if < 0"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
