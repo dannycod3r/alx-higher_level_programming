e594c63a344dc81f326882ff1b75f3e745aa2a31
dannycod3r <oseid7807@gmail.com>
2023-06-19 00:24:18 +0000

    Add docs to test method

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,6 +7,8 @@ from models.rectangle import Rectangle
 class TestRectangleClass(unittest.TestCase):
     """Test class for class Rectangle"""
     def test_initialization_rectangle_auto_set_id(self):
+        """Test initialisation with and without id\
+        should set respective attribute in the constructor"""
         self.assertEqual(Rectangle().id, 1)
         self.assertEqual(Rectangle(2, 10).id, 2)
         self.assertEqual(Rectangle(10, 2).id, 3)
