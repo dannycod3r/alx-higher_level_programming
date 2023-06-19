56cfb0e866de138532e7432f3c21200b1af92a24
dannycod3r <oseid7807@gmail.com>
2023-06-19 00:34:24 +0000

    Update test file for rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,6 +9,6 @@ class TestRectangleClass(unittest.TestCase):
     def test_initialization_rectangle_auto_set_id(self):
         """Test initialisation with and without id\
         should set respective attribute in the constructor"""
-        self.assertEqual(Rectangle(2, 10).id, 1) # no id, id = 1
-        self.assertEqual(Rectangle(10, 2).id, 2) # no id, id = 2
-        self.assertEqual(Rectangle(10, 2, 0, 0, 12).id, 12) # id = 12
+        self.assertEqual(Rectangle(2, 10).id, 1)  # no id, id = 1
+        self.assertEqual(Rectangle(10, 2).id, 2)  # no id, id = 2
+        self.assertEqual(Rectangle(10, 2, 0, 0, 12).id, 12)  # id = 12
