b1f77b2fff2551f430bb8348f607a9645304bca9
dannycod3r <oseid7807@gmail.com>
2023-07-18 17:15:32 -0700

    Intro to SQL, MySQL

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- A script that creates the database hbtn_0c_0 in your MySQL server
+-- The script should not fail if the database exists
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- A script that deletes the database hbtn_0c_0 in your MySQL server
+-- The script should not fail if the database doesnt exit
+DROP DATABASE IF EXISTS hbtn_0c_0
+
