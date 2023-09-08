1158e37b177693d1317edca0aa1d336bf0f9572f
dannycod3r <oseid7807@gmail.com>
2023-09-08 14:10:24 +0000

    Remove strip

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,8 +12,7 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    text = text.strip()\
-        .replace(".", ".\n\n")\
+    text = text.replace(".", ".\n\n")\
         .replace("?", "?\n\n")\
         .replace(":", ":\n\n")
 
