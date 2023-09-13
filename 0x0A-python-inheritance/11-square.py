17e05707a3a94395239483c7c1771be04e62a9f6
dannycod3r <oseid7807@gmail.com>
2023-09-13 22:45:34 +0000

    Correct str method to displace Square instead of Rectangle

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 10-square supplies the class Square"""
+Rectangle = __import__('10-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """
+
+        """
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def __str__(self):
+        """Return an informal representation of Rectangle"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
+
+    def area(self):
+        """Override area method"""
+        return self.__size ** 2
