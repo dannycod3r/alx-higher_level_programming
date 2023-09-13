1682fec63d62243af596642984aa0ff36d0ea385
dannycod3r <oseid7807@gmail.com>
2023-09-13 13:49:32 +0000

    Add function that raises an exception when called

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module supplies empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+    def area(self):
+        """area method Not implemented
+
+        Raises:
+            Exception: area not implemented
+        """
+        raise Exception("area() is not implemented")
