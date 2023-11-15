eef7a55ad33772c4586bcbbc2c964ea791584873
dannycod3r <oseid7807@gmail.com>
2023-11-15 01:52:21 +0000

    Update code to meet requirement

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  // Define a recursive function to convert a number to the specified base
-  const convertToBase = function (number) {
-    if (number < base) {
-      // B: if the number is less than the base
-      return '0123456789abcdef'[number];
-    } else {
-      // Recursive: convert the quotient and append the remainder as a hex digit
-      return convertToBase(Math.floor(number / base)) + hexDigits[number % base];
-    }
+  return function (num) {
+    return num.toString(base);
   };
-
-  // Return the inner function for external use
-  return convertToBase;
 };
