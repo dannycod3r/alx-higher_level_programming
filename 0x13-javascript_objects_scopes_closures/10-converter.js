b3f71ac723289304ac338fbf7c9c6d44677bf700
dannycod3r <oseid7807@gmail.com>
2023-11-15 01:36:47 +0000

    Remove variable declaration to satisfy requirement:

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,11 +3,9 @@
 exports.converter = function (base) {
   // Define a recursive function to convert a number to the specified base
   const convertToBase = function (number) {
-    const hexDigits = '0123456789abcdef';
-
     if (number < base) {
       // B: if the number is less than the base
-      return hexDigits[number];
+      return '0123456789abcdef'[number];
     } else {
       // Recursive: convert the quotient and append the remainder as a hex digit
       return convertToBase(Math.floor(number / base)) + hexDigits[number % base];
