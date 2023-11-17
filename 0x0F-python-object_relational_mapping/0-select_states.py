2a448fc276cd89d24a6bf6ae3d0d0cc5019a591a
dannycod3r <oseid7807@gmail.com>
2023-11-17 03:18:04 -0800

    Add script that lists all states from database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the
+database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+# database details from commandline
+args = sys.argv[1:]
+mysql_username = args[0]
+mysql_password = args[1]
+database_name = args[2]
+db_host = "localhost"
+port = 3306
+
+# connect to the database
+db = MySQLdb.connect(
+    host=db_host,
+    user=mysql_username,
+    passwd=mysql_password,
+    db=database_name,
+    port=port
+)
+
+cur = db.cursor()
+cur.execute("SELECT * FROM states")  # SQL query
+states = cur.fetchall()
+
+for state in states:
+    print(state)
+
+# close connections
+cur.close()
+db.close()
