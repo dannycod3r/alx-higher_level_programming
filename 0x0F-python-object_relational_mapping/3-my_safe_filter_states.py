48d4b56e6492ba940f5cfc7730af49ab3451415a
dannycod3r <oseid7807@gmail.com>
2023-11-20 08:33:08 -0800

    Use parametised queries to prevent SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+A script that lists all states with name
+same as searched state from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # database details from commandline
+    args = sys.argv[1:]
+    mysql_username = args[0]
+    mysql_password = args[1]
+    database_name = args[2]
+    state_searched = args[3]
+    db_host = "localhost"
+    port = 3306
+
+    # connect to the database
+    db = MySQLdb.connect(
+        host=db_host,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        port=port
+    )
+
+    cur = db.cursor()
+    # SQL query
+    cur.execute("SELECT * FROM states WHERE name \
+                 LIKE BINARY %s ORDER BY id ASC",(state_searched,))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    # close connections
+    cur.close()
+    db.close()
