aaa1a543b9c0afff1a1b00fd0fe5e6d5bcfd86c8
dannycod3r <oseid7807@gmail.com>
2023-11-21 22:20:54 -0800

    Change to states.name not state.name

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     # SQL query
-    cur.execute("SELECT cities.id, cities.name, state.name \
+    cur.execute("SELECT cities.id, cities.name, states.name \
                  FROM cities JOIN states ON cities.state_id \
                  = states.id ORDER BY cities.id ASC")
     cities = cur.fetchall()
