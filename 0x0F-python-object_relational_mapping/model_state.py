8b2eb123bc7646fe69cc49237c65589acde8eff4
dannycod3r <oseid7807@gmail.com>
2023-11-24 11:12:39 +0000

    Add state model that contains definition for state table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module contains class definition for State
+Base - instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Database model for State
+    
+    Attributes:
+    __tablename__: table name
+    id: state id
+    name: state name
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
