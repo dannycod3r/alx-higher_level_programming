f358724a71851bab1dbc0d2fd7d5e8f2569d0341
dannycod3r <oseid7807@gmail.com>
2023-12-01 14:07:19 +0000

    Add if __name__ check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,15 +4,17 @@ Script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request as requests
 
-with requests.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    display = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(
-        type(content),
-        content,
-        content.decode('utf-8')
-    )
+if __name__ == '__main__':
+    
+    with requests.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        display = """Body response:
+        - type: {}
+        - content: {}
+        - utf8 content: {}""".format(
+            type(content),
+            content,
+            content.decode('utf-8')
+        )
 
-    print(display)
+        print(display)
