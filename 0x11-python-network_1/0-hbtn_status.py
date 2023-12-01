d3b8e875eefd4f99eb37adcf6da10f832ae1d392
dannycod3r <oseid7807@gmail.com>
2023-12-01 11:29:32 +0000

    Add script that fetches from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request as requests
+
+with requests.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    content = response.read()
+    display = """Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(
+        type(content),
+        content,
+        content.decode('utf-8')
+    )
+
+    print(display)
