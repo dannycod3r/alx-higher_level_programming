619084b1df0397e19a750da59b54fb77409e7951
dannycod3r <oseid7807@gmail.com>
2024-01-11 14:56:18 +0000

    Remove space after the tab delimiter

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     req = requests.get('https://alx-intranet.hbtn.io/status')
 
     print('Body response:')
-    print('\t - type: {}'.format(type(req.text)))
-    print('\t - content: {}'.format(req.text))
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
